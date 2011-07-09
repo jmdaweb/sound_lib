2b63b755c405f7dda95e651e85a6fb8e30a9e808
Q <q@qwitter-client.net>
2011-07-09 11:06:46 -0400

    Added access to the rate ratio attribute to tempo channels

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from glob import glob
+import sys
+
+__name__ = 'sound_lib'
+__author__ = 'Christopher Toth'
+__version__ = '0.6.5'
+
+setup(
+ name = __name__,
+ author = __author__,
+ author_email = 'q@qwitter-client.net',
+ version = __version__,
+ url = 'http://www.qwitter-client.net',
+ description = 'Pythonic wrapper to the Bass sound library',
+ #long_description = open('README.txt').read(),
+ package_dir = {'sound_lib':'sound_lib'},
+ packages = find_packages(),
+ package_data = {"sound_lib":
+  [
+   "lib/bass/*",
+   "lib/bass_fx/*",
+   "lib/bassenc/*",
+   "lib/bassflac/*",
+  ],
+ },
+ classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Developers',
+  #'Operating System :: Microsoft :: Windows',
+  'Programming Language :: Python',
+  'License :: OSI Approved :: MIT License',
+'Topic :: Software Development :: Libraries'
+],
+)
---
+++ sound_lib/stream.py
@@ -0,0 +1,59 @@
+from channel import Channel
+from main import bass_call
+from external.pybass import *
+from external.pybassflac import *
+
+
+FILETYPE_HANDLERS = {
+ '.flac': BASS_FLAC_StreamCreateFile,
+}
+
+
+class BaseStream(Channel):
+
+ def _callback(*args):
+  #Stub it out as otherwise it'll crash, hard.  Used for stubbing download procs
+  return 0
+ 
+ def free(self):
+  return bass_call(BASS_StreamFree, self.handle)
+
+class Stream(BaseStream):
+
+ def __init__(self, freq=44100, chans=2, flags=0, proc=None, user=None, three_d=False, autofree=False, decode=False):
+  self.proc = STREAMPROC(proc)
+  self.setup_flag_mapping()
+  flags = flags | self.flags_for(three_d=three_d, autofree=autofree, decode=decode)
+  handle = bass_call(BASS_StreamCreate, freq, chans, flags, self.proc, user)
+  super(Stream, self).__init__(handle)
+
+class FileStream(BaseStream):
+
+ def __init__(self, mem=False, file=None, offset=0, length=0, flags=0, three_d=False, mono=False, autofree=False, decode=False, unicode=False):
+  """Creates a sample stream from an MP3, MP2, MP1, OGG, WAV, AIFF or plugin supported file."""
+  self.setup_flag_mapping()
+  flags = flags | self.flags_for(three_d=three_d, autofree=autofree, mono=mono, decode=decode, unicode=unicode)
+  self.file = file
+  stream_creator = BASS_StreamCreateFile
+  for k in FILETYPE_HANDLERS:
+   if hasattr(file, 'lower') and file.lower().endswith(k):
+    stream_creator = FILETYPE_HANDLERS[k]
+    break
+  handle = bass_call(stream_creator, mem, file, offset, length, flags)
+  super(FileStream, self).__init__(handle)
+
+ def setup_flag_mapping(self):
+  super(FileStream, self).setup_flag_mapping()
+  self.flag_mapping.update({
+   'unicode': BASS_UNICODE
+  })
+
+class URLStream(BaseStream):
+
+ def __init__(self, url="", offset=0, flags=0, downloadproc=None, user=None):
+  self._downloadproc = downloadproc or self._callback #we *must hold on to this
+  self.downloadproc = DOWNLOADPROC(self._downloadproc)
+  self.url = url
+  handle = bass_call(BASS_StreamCreateURL, url, offset, flags, self.downloadproc, user)
+  super(URLStream, self).__init__(handle)
+
